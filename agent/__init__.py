b5c6ac6c168678d3992de4738235b04459e6118c
tech-envision <tech@envisionmindcare.com>
2025-06-16 23:06:58 -0600

    Restrict mini agent API

--- agent/__init__.py
+++ agent/__init__.py
@@ -1,10 +1,5 @@
 from .chat import ChatSession
-from .sessions.team import (
-    TeamChatSession,
-    send_to_junior,
-    send_to_junior_async,
-    set_team,
-)
+from .sessions.team import TeamChatSession, set_team
 from .sessions.solo import SoloChatSession
 from .simple import (
     solo_chat,
@@ -34,8 +29,6 @@ __all__ = [
     "TeamChatSession",
     "execute_terminal",
     "execute_terminal_async",
-    "send_to_junior",
-    "send_to_junior_async",
     "set_team",
     "set_vm",
     "execute_with_secret",
