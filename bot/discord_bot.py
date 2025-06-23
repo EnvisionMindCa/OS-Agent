248b43da35d1494983d016547faa35e169caf576
starsnatched <minchyeom@gmail.com>
2025-06-23 13:24:36 -0600

    Set WSConnection think parameter to True in _get_connection method

--- bot/discord_bot.py
+++ bot/discord_bot.py
@@ -473,7 +473,7 @@ class DiscordTeamBot(commands.Bot):
         key = (user, session)
         conn = self._connections.get(key)
         if conn is None:
-            conn = WSConnection(self._client, user=user, session=session, think=False)
+            conn = WSConnection(self._client, user=user, session=session, think=True)
             await conn.connect()
             self._connections[key] = conn
             asyncio.create_task(self._relay_messages(conn, channel))
